342b6168ee914143b64a0d5c281e9e097282d049
Deivison <deivison.francisco@atstrans.onmicrosoft.com>
2020-08-25 22:19:23 -0300

    ##5

--- src/controllers/cep/cep-controller.js
+++ src/controllers/cep/cep-controller.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-console */
 /* eslint-disable no-plusplus */
 import CepService from '../../service/cep';
 import NotFoundError from '../../lib/errors/not-found-error';
 import { findCEPMock } from '../../helper/cep-helper';
+import InternalServerError from '../../lib/errors/internal-server-error';
 
 class Cep {
   async cepSearch(req, res) {
@@ -14,19 +16,20 @@ class Cep {
       if (result && !result.data) {
         result = await CepService.search(cep);
       }
+
       const { data } = result;
+
       if (data && !data.erro) {
         res.status(200).json(data);
         return;
       }
       res.status(404).json(new NotFoundError());
     } catch (error) {
-      console.log(error);
-      const status = error.response.status || error.status;
+      const status = (error && error.response && error.response.status) || error.status;
       if (status === 400 || status === 404) {
         res.status(status).json(new NotFoundError());
       } else {
-        res.status(status).json(error);
+        res.status(500).json(new InternalServerError());
       }
     }
   }
--- src/helper/cep-helper.js
+++ src/helper/cep-helper.js
@@ -1,81 +1,16 @@
+/* eslint-disable no-console */
 /* eslint-disable object-curly-newline */
 /* eslint-disable radix */
 /* eslint-disable no-plusplus */
 /* eslint-disable import/prefer-default-export */
 
-const cepsMocks = [
-  {
-    cep: 22333999,
-    logradouro: '',
-    complemento: '',
-    bairro: 'Bairro 01',
-    localidade: 'Localidade 01',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  },
-  {
-    cep: 22333990,
-    logradouro: '',
-    complemento: '',
-    bairro: 'Bairro 01',
-    localidade: 'Localidade 01',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  },
-  {
-    cep: 22333900,
-    logradouro: '',
-    complemento: '',
-    bairro: 'Bairro 01',
-    localidade: 'Localidade 01',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  },
-  {
-    cep: 22333000,
-    logradouro: '',
-    complemento: '',
-    bairro: 'Bairro 01',
-    localidade: 'Localidade 01',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  },
-  {
-    cep: 22330000,
-    logradouro: '',
-    complemento: '',
-    bairro: 'Bairro 01',
-    localidade: 'Localidade 01',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  },
-  {
-    cep: 22300000,
-    logradouro: 'Rua 05',
-    complemento: '',
-    bairro: 'Recife 05',
-    localidade: 'Localidade 05',
-    uf: 'PE',
-    ibge: '',
-    gia: '',
-    ddd: '81'
-  }
-];
+import cepsMocks from './cep-mock-list';
 
 export const findCEPMock = (cepOriginal) => {
   const sizeCEP = cepOriginal.toString().length + 1;
   let result = {};
   let cont = 0;
+
   for (let index = 0; index < sizeCEP; index++) {
     let cepForSearch = cepOriginal.toString();
 
@@ -83,7 +18,7 @@ export const findCEPMock = (cepOriginal) => {
       const valorSlice = cepForSearch.slice(0, -cont);
       cepForSearch = valorSlice.padEnd(sizeCEP - 1, '0');
     }
-    const cepFilter = cepsMocks.filter(
+    const cepFilter = cepsMocks().filter(
       (cepData) => cepData.cep === parseInt(cepForSearch) && cepData.logradouro !== ''
     );
     if (cepFilter && cepFilter.length === 1) {
@@ -107,5 +42,4 @@ export const findCEPMock = (cepOriginal) => {
     }
   }
   return result;
-  //cepsMocks;
 };
